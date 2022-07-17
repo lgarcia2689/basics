1c46e4d16a0bc93237c0151a5d83fd80958bfd77
Luis Garcia <jrluis21@gmail.com>
2022-07-17 16:39:13 -0500

    made some changes and wanted to save

--- app.py
+++ app.py
@@ -1,7 +1,26 @@
+# https://www.youtube.com/watch?v=_uQrJ0TkZlc
+# I am following along on this video
+
 # print("Luis Garcia")
 # print("0----")
 # print(" ||||")
 # print("*" * 10)
+# price = 10
+# rating = 4.9
+# name = "Mosh"
+# is_published = False
+# name = "John Smith"
+# age = 20
+# is_new = True
+# name = input('What is your name? ')
+# color = input('What is you favorite color? ')
+# print(name + ' likes ' + color)
+# birth_year = input('Birth year: ')
+# print(type(birth_year))
+# age = 2022 - int(birth_year)
+# print(type(age))
+# print(age)
 
-price = 10
-print (price)
+pounds = input('What is you weight (in pounds)? ')
+kilo = int(pounds) * 0.45
+print(kilo)
